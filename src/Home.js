fe48bfd9d2e8e1214790e07d40f03b404d19bcf1
Lisandro Neris <lisandroneris07@gmail.com>
2021-12-15 22:39:48 -0500

    Created baseline for Todo app

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import Nav from './Nav';
 import './App.css';
+import react from 'react';
 
 
 
-function Home() {
-  
- return <div> 
+class Home extends React.Component {
+  constructor(props){
+    super (props);
+    this.state={
+items:[],
+currentItem:{
+  text:'',
+  key:''
+}
+    }
+  }
+
+ render() {
+   return ( <div> 
    <Nav/>
             <h1 className= "homeTitle"> Welcome To Your To-Do App </h1>
             
     
+  <div className = "main">
+
+
+ <header> 
+   <form id= "to-do-form"> 
+   <input type= "text" placeholder = "Enter Text"/>
+   <button type= "submit"> Add </button>
+
+   </form>
+ </header>
+
+  </div>
+
   
  </div>
+   )
+ }
 
      }
      
